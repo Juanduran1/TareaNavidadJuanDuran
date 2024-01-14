1912f0585d4223c55a8ec1b7f671ef6e499212f9
Juan Duran <juanduranhijo@gmail.com>
2024-01-14 14:18:46 +0100

    a

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+
+import "@fortawesome/fontawesome-free/js/all.js";
+
+import { getSlider } from 'simple-slider';
+
+ 
+getSlider();
+
+
+
+
+console.log("plantilla de Juan que se lo tiene muy creido, y tarea de Juan Durán que se lo tiene mas creido aún");
+
+
